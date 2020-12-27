e9ecb997eb103519a1fed6149a72b4bb814e87d9
harounans <hansari97@gmail.com>
2020-12-27 10:03:54 -0500

    add reveal screen

--- src/Components/Mobile/CaptainScreen.jsx
+++ src/Components/Mobile/CaptainScreen.jsx
@@ -1,30 +1,81 @@
 import React, { Component } from 'react'
 import { MobileContext } from './MobileContext';
+import Loading from './Loading';
 
 export default class CaptainScreen extends Component {
     static contextType = MobileContext;
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            activePlayers: []
+        };
+    }
 
     createListGroup = () => {
         const { gameState } = this.context;
         const { players } = gameState;
-        
+        const { activePlayers } = this.state;
+
         const listItems = players.map((player) =>
-            <li className="list-group-item">{player.name}</li>
+            <li className={activePlayers.includes(player.name) ? "list-group-item active" : "list-group-item list-group-item-dark"}
+                onClick={() => this.toggleActivePlayer(player.name)}>
+                {player.name}
+            </li>
         );
 
         return (
-            <ul className="list-group" style={{margin: '10px'}}>{ listItems }</ul>
+            <ul className="list-group" style={{ margin: '10px', textAlign: 'left' }}>{listItems}</ul>
         )
     }
-    render() {
+
+    toggleActivePlayer = player => {
         const { gameState } = this.context;
-        const { players, board, turn } = gameState;
+        const { board, turn } = gameState;
         const numPlayersOnMission = board[turn];
 
+        let { activePlayers } = this.state;
+        if (activePlayers.includes(player)) {
+            activePlayers.splice(activePlayers.indexOf(player), 1);
+        }
+        else {
+            if (activePlayers.length >= numPlayersOnMission) {
+                return;
+            }
+
+            activePlayers.push(player);
+        }
+
+        this.setState({ activePlayers }, () => console.log(this.state));
+    }
+
+    chooseTeam = () => {
+        const sessionId = this.context.sessionId;
+        console.log("Choosing Team for " + sessionId);
+
+        //Send Message
+        this.context.ws.json({
+            action: "chooseTeam",
+            sessionId,
+            team: this.state.activePlayers
+        });
+        this.setState({ loading: true });
+    }
+
+    render() {
+        const { activePlayers, loading } = this.state;
+        const { gameState } = this.context;
+        const { players, board, turn } = gameState;
+
+        if (loading)
+            return <Loading />
+
+        const numPlayersOnMission = board[turn];
         return (
-            <div>
-                pick {numPlayersOnMission} to go on the mission
-                { this.createListGroup() }
+            <div className="centered">
+                <span style={{ color: "white" }}>pick {numPlayersOnMission} to go on the mission</span>
+                {this.createListGroup()}
+                {activePlayers.length === numPlayersOnMission && <button className="playbtn" type="button" onClick={this.chooseTeam}>CHOOSE TEAM</button>}
             </div>
         )
     }
---
+++ src/Components/Mobile/RevealScreen.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { MobileContext } from './MobileContext';
+
+class RevealScreen extends Component {
+    static contextType = MobileContext;
+
+    isSpy = () => {
+        const { playerName, gameState } = this.context;
+        for (let player of gameState.players) {
+            if (player.name === playerName) {
+                return player.isSpy;
+            }
+        }
+    }
+
+    spyList = () => {
+        const { playerName, gameState } = this.context;
+        let teammates = [];
+        for (let player of gameState.spies) {
+            if (player === playerName) {
+                continue;
+            }
+            teammates.push(<p>➤ {player}</p>)
+        }
+        return (
+            <div>
+                <h5>Your Teammates:</h5>
+                {teammates}
+            </div>
+        )
+    }
+
+    render() {
+        if (this.isSpy()) {
+            return (
+                <div className="centered">
+                    <h1>You are a SPY</h1>
+                    {this.spyList()}
+                </div>
+            )
+        }
+
+        return (
+            <div className="centered">
+                <h1>You are RESISTANCE</h1>
+            </div>
+        );
+    }
+}
+
+export default RevealScreen;
